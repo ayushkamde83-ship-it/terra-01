e6b495bf8bccf5e65e98ced1bb5026fc9927149d
ayushkamde83-ship-it <ayushkamde83@gmail.com>
2026-03-23 08:29:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 # -------------------------
 # Provider
-# -------------------------
+# -----------------------
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
 
 provider "aws" {
   region = "ap-south-1"
